ee74e3584fa3ec5e65450825b4b4e9620a563fba
iamanx17 <pranj.17am@gmail.com>
2021-02-26 12:14:15 +0530

    product

--- index/views.py
+++ index/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse,JsonResponse
 from .models import products,category
 # Create your views here.
 
@@ -10,3 +11,22 @@ def home(request):
         pp=products.objects.filter(product_cate=i['id'])
         product.append([i['cate'],pp])
     return render(request,'index.html',{'pro':product})
+
+def viewproduct(request,id):
+    pro=products.objects.filter(id=id)
+    recpro=products.objects.filter(product_cate=id).exclude(id=id)
+    print(recpro)
+
+    return render(request,'product.html',{'pro':pro[0],'recpro':recpro})
+
+def json(request):
+    data={
+        'Email':'Pran.17am@gmail.com',
+        'Username':'iamanx17',
+        'Password':'xxxx',
+
+    
+    
+    
+    }
+    return JsonResponse(data, safe=False)
